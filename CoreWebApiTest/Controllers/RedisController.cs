86b19648c2065d7e26fa8ae36e61dcbd27c8ead0
Benson Hsieh <benson_hsieh@syscom.com.tw>
2019-02-11 17:14:20 +0800

    First commit

---
+++ CoreWebApiTest/Controllers/RedisController.cs
@@ -0,0 +1,27 @@
+﻿using CoreWebApiTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace CoreWebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        public RedisConfig _RedisConfig { get; }
+        public RedisController(Microsoft.Extensions.Options.IOptions<RedisConfig> redisConfig)
+        {
+            _RedisConfig = redisConfig.Value;
+        } //Action Controller
+        [HttpGet("{val}")]
+        public string Get(string val)
+        {
+            ConnectionMultiplexer redisConn = ConnectionMultiplexer.Connect($"{_RedisConfig.IP},password={_RedisConfig.Pass}");
+            //ConnectionMultiplexer redisConn = ConnectionMultiplexer.Connect("127.0.0.1:6379,password=syscom#1@2");
+            IDatabase resDb = redisConn.GetDatabase();
+            resDb.StringSet("testKey", val);
+            var result = resDb.StringGet("testKey");
+            return result.ToString();
+        }
+    }
+}
---
+++ CoreWebApiTest/Model/RedisConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApiTest.Model
+{
+    public class RedisConfig
+    {
+        public string IP { get; set; }
+        public string Pass { get; set; }
+    }
+}
